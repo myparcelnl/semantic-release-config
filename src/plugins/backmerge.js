4351f1a1881bbc41960897baeaff18f68d48f237
Edie Lemoine <edie@myparcel.nl>
2021-09-29 17:10:29 +0200

    feat: add backmerge plugin

---
+++ src/plugins/backmerge.js
@@ -0,0 +1,9 @@
+const backmerge = [
+  '@saithodev/semantic-release-backmerge',
+  {
+    branchName: 'develop',
+    message: 'chore(release): rebase published changes [skip ci]',
+  },
+];
+
+module.exports = {backmerge};
