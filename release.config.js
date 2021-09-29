461592e04396a0059b153e56ef0b3a0de2e9e858
Edie Lemoine <edie@myparcel.nl>
2021-09-29 15:16:43 +0200

    ci: update release configuration

--- release.config.js
+++ release.config.js
@@ -1,3 +1,13 @@
+const baseConfig = require('./index');
+const {git} = require('./src/plugins/git');
 const npmConfig = require('./npm');
 
-module.exports = npmConfig;
+module.exports = {
+  ...baseConfig,
+  ...npmConfig,
+  plugins: [
+    ...npmConfig.plugins,
+    git,
+  ],
+  extends: null,
+};
