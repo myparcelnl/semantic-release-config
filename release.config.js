b705cd9cdea50138fb9ae5127d5bef3b201f3fc4
Edie Lemoine <edie@myparcel.nl>
2021-10-04 13:39:16 +0200

    build: change master to main

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
+const baseConfig = require('./index');
 const gitHubConfig = require('./github');
 const npmConfig = require('./npm');
 
 module.exports = {
+  ...baseConfig,
   extends: null,
   plugins: [
     ...npmConfig.plugins,
