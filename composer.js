afe605858dfdca7a96aca6e8f3971225da93a57b
Edie Lemoine <edie@myparcel.nl>
2021-12-31 11:39:52 +0100

    fix(composer): add missing properties from main config

--- composer.js
+++ composer.js
@@ -3,6 +3,7 @@ const mainConfig = require('./index');
 const {addComposerPlugin} = require('./src/plugins/addComposerPlugin');
 
 module.exports = {
+  ...mainConfig,
   extends: '@myparcel/semantic-release-config',
   plugins: [
     ...mainConfig.plugins,
