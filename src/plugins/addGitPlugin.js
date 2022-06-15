3732b6d2e53d5c82902d1d822e537f1d4083c26c
Edie Lemoine <edie@myparcel.nl>
2022-06-15 16:18:17 +0200

    fix(git): add yarn and pnpm lockfiles to default assets

--- src/plugins/addGitPlugin.js
+++ src/plugins/addGitPlugin.js
@@ -1,13 +1,15 @@
-const {addPlugin} = require('../helpers/addPlugin');
+ const {addPlugin} = require('../helpers/addPlugin');
 
 const defaults = {
   assets: [
     'CHANGELOG.md',
-    'package.json',
-    'package-lock.json',
-    'npm-shrinkwrap.json',
     'composer.json',
     'composer.lock',
+    'npm-shrinkwrap.json',
+    'package-lock.json',
+    'package.json',
+    'pnpm-lock.yaml',
+    'yarn.lock',
   ],
   message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 };
